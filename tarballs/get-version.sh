d0ce71aa4b83ac4a264403f24ea1c37093efef86
Piero Proietti <piero.proietti@gmail.com>
2025-02-27 11:44:04 +0100

    ritocco in tarballs

--- tarballs/get-version.sh
+++ tarballs/get-version.sh
@@ -5,4 +5,4 @@ else
     VERSION="$1"
 fi
 VERSIONS=$1
-wget https://drive.google.com/drive/folders/1ECZnKQg4r08TyUT9yHPsZMlzuNeLP5e5/penguins-eggs-tarball-${VERSION}-1-linux-x64.tar.gz
+wget https://penguins-eggs.net/basket/index.php?p=packages%2Ftarballs&view=penguins-eggs-tarball-1${VERSION}-linux-x64.tar.gz
