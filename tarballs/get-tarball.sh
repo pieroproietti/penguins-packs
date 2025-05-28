cefcdccdf5b65f1b6d060e4ebc9cce13a9f4e656
artisan <artisan@minimal>
2025-05-28 18:26:34 +0200

    updating tarball install

--- tarballs/get-tarball.sh
+++ tarballs/get-tarball.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 if [ -z "$1" ]; then
-    VERSION="10.0.59"
+    VERSION="10.1.1"
 else
     VERSION="$1"
 fi
 VERSIONS=$1
-#wget https://penguins-eggs.net/basket/index.php?p=packages%2Ftarballs&get=penguins-eggs-tarball-1${VERSION}-linux-x64.tar.gz
-wget https://penguins-eggs.net/basket/packages/tarballs/penguins-eggs-tarball-10.0.59-1-linux-x64.tar.gz
+wget https://penguins-eggs.net/basket/packages/tarballs/penguins-eggs-tarball-10.1.1-1-linux-x64.tar.gz
