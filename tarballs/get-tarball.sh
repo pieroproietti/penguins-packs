62823a99fe005197641744d712ffc8b769e54323
artisan <artisan@minimal>
2025-05-28 19:04:04 +0200

    ritocco

--- tarballs/get-tarball.sh
+++ tarballs/get-tarball.sh
@@ -5,4 +5,4 @@ else
     VERSION="$1"
 fi
 VERSIONS=$1
-wget https://penguins-eggs.net/basket/packages/tarballs/penguins-eggs-tarball-10.1.1-1-linux-x64.tar.gz
+wget https://penguins-eggs.net/basket/packages/tarballs/penguins-eggs-10.1.1-1-linux-x64.tar.gz
