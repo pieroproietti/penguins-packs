7a547a35376e4d7557507caac0d1cb88c5a50db4
artisan <artisan@minimal>
2025-05-29 05:49:30 +0200

    removed tarball in favor of get-eggs

--- tarballs/get-tarball.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ -z "$1" ]; then
-    VERSION="10.1.1"
-else
-    VERSION="$1"
-fi
-VERSIONS=$1
-wget https://penguins-eggs.net/basket/index.php/packages/penguins-eggs_10.1.1-1-linux-x64.tar.gz
