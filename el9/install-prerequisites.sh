700e12ad01e8b724edba0b93c5cdff9ecf8a7ef2
Piero Proietti <piero.proietti@gmail.com>
2025-07-22 08:31:08 +0200

    da testare

--- el9/install-prerequisites.sh
+++ el9/install-prerequisites.sh
@@ -5,8 +5,5 @@ sudo dnf config-manager --set-enabled crb
 echo "Adding rpm.nodesource.com... "
 curl -fsSL https://rpm.nodesource.com/setup_20.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
-sudo rm nodesource_setup.sh
+rm nodesource_setup.sh
 sudo dnf check-update
-
-
-
