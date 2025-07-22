92f6b288f0695be594904b7621ee0f1792b96dfc
Piero Proietti <piero.proietti@gmail.com>
2025-07-22 07:56:06 +0200

    crb

--- el9/install-prerequisites.sh
+++ el9/install-prerequisites.sh
@@ -1,6 +1,10 @@
 echo "Adding epel-release..."
 sudo dnf install epel-release -y
+dnf config-manager --set-enabled crb
+dnf -y update
+dnf config-manager --set-enabled crb
 echo "Adding rpm.nodesource.com... "
 curl -fsSL https://rpm.nodesource.com/setup_20.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
+
 
